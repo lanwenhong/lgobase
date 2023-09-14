f60798af76627e70750109188f129c6e2c78c7eb
lanwenhong <hexiexuanlv@126.com>
2023-09-14 09:47:35 +0800

    change ctx

--- dlock/dlock.go
+++ dlock/dlock.go
@@ -21,10 +21,10 @@ type Dlock struct {
 	Ltime int64
 	Rrp   *redispool.RedisPool
 	Flag  string
-	Ctx   context.Context
+	//Ctx   context.Context
 }
 
-func DlockNew(ctx context.Context, rrp *redispool.RedisPool, lkey string) (dlock *Dlock, err error) {
+func DlockNew(rrp *redispool.RedisPool, lkey string) (dlock *Dlock, err error) {
 	dlock = new(Dlock)
 	dlock.Key = lkey
 	dlock.Rrp = rrp
@@ -36,24 +36,24 @@ func DlockNew(ctx context.Context, rrp *redispool.RedisPool, lkey string) (dlock
 	return dlock, err
 }
 
-func (dl *Dlock) Lock() error {
+func (dl *Dlock) Lock(ctx context.Context) error {
 	for {
 		t := time.Now().UnixNano()
 		//st := fmt.Sprintf("%d", t)
 		logger.Debugf(dl.Ctx, "%s try lock", dl.Flag)
-		ret, err := dl.Rrp.Do(dl.Ctx, "set", dl.Key, t, "nx", "px", KEY_EX_TIME)
+		ret, err := dl.Rrp.Do(ctx, "set", dl.Key, t, "nx", "px", KEY_EX_TIME)
 		if err != nil {
 			return err
 		} else {
 			st, _ := redis.String(ret, err)
-			logger.Debugf(dl.Ctx, "flag %s try lock st: %s", dl.Flag, st)
+			logger.Debugf(ctx, "flag %s try lock st: %s", dl.Flag, st)
 			if st == "OK" {
 				//set lock start time
 				dl.Ltime = t
 				break
 			} else {
 				//sleep wait unlock
-				logger.Debugf(dl.Ctx, "%s wait", dl.Flag)
+				logger.Debugf(ctx, "%s wait", dl.Flag)
 				time.Sleep(time.Duration(KEY_WAIT_TIME) * time.Millisecond)
 			}
 		}
@@ -61,22 +61,22 @@ func (dl *Dlock) Lock() error {
 	return nil
 }
 
-func (dl *Dlock) Unlock() error {
-	ret, err := dl.Rrp.Do(dl.Ctx, "get", dl.Key)
+func (dl *Dlock) Unlock(ctx context.Context) error {
+	ret, err := dl.Rrp.Do(ctx, "get", dl.Key)
 	if err != nil {
 		return err
 	}
 	set_t, _ := redis.Int64(ret, err)
 	etime := time.Now().UnixNano()
-	logger.Debugf(dl.Ctx, "%s etime: %d lock_time: %d redis_time: %d", dl.Flag, etime, dl.Ltime, set_t)
+	logger.Debugf(ctx, "%s etime: %d lock_time: %d redis_time: %d", dl.Flag, etime, dl.Ltime, set_t)
 	if etime-dl.Ltime > KEY_EX_TIME*1000*1000 && etime-set_t < KEY_EX_TIME*1000*1000 {
-		logger.Debugf(dl.Ctx, "%s timout and found new lock", dl.Flag)
+		logger.Debugf(ctx, "%s timout and found new lock", dl.Flag)
 		return nil
 	} else if set_t == 0 {
-		logger.Debugf(dl.Ctx, "---%s timeout", dl.Flag)
+		logger.Debugf(ctx, "---%s timeout", dl.Flag)
 		return nil
 	}
-	logger.Debugf(dl.Ctx, "%s release", dl.Flag)
-	_, err = dl.Rrp.Do(dl.Ctx, "del", dl.Key)
+	logger.Debugf(ctx, "%s release", dl.Flag)
+	_, err = dl.Rrp.Do(ctx, "del", dl.Key)
 	return err
 }
